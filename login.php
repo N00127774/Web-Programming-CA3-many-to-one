7c48c748426e19b058b22597fd9f2df8a7e927a2
Baabatunde Adegboye <Adegboyetunde24@yahoo.ie>
2015-03-18 20:58:26 +0000

    Today i WORKED ON the register Page and also the Login page adding style and icons to them

--- login.php
+++ login.php
@@ -1,65 +1,137 @@
 <!DOCTYPE html>
 <html>
     <head>
+        
+        
+        
 
-        <meta charset="UTF-8">
-        <title></title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+      
+        <meta charset="utf-8">
+        <title>My Website</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+        <script src="js/respond.js"></script>
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/font-awesome.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="styles/ihover.css" rel="stylesheet"> <!-- for the images and effects on them-->
+        <link href="styles/ihover.min.css" rel="stylesheet"> 
+        <link href='http://fonts.googleapis.com/css?family=Lato:400,900|Kreon' rel='stylesheet' type='text/css'>
+
+        <script type="text/javascript" src="js/customer.js"></script>
+        <link rel="icon" type="image/x-icon" href="Images/logo.ico">
+       
     </head>
-    <body>
+    <body class="BackgroundImage">
         <?php
         if (!isset($username)) {
             $username = '';
         }
         ?>
-        <form action="checkLogin.php" method="POST">
-            <table border="1">
-                <tbody>
-                    <tr>
-                        <th>Username</th>
-                        <td>
-                            <input type="text"
-                                   name="username"
-                                   value="<?php echo $username; ?>" />
-                            <span id="usernameError" class="error">
-                                <?php
-                                if (isset($errorMessage) && isset($errorMessage['username'])) {
-                                    echo $errorMessage['username'];
-                                }
-                                ?>
-                            </span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Password</td>
-                        <td>
-                            <input type="password" name="password" value="" />
-                            <span id="passwordError" class="error">
-                                <?php
-                                if (isset($errorMessage) && isset($errorMessage['password'])) {
-                                    echo $errorMessage['password'];
-                                }
-                                ?>
-                            </span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>
-                            <input type="submit" value="Login" name="login" />
-                            <input type="button"
-                                   value="Register"
-                                   name="register"
-                                   onclick="document.location.href = 'register.php'"
-                                   />
-                            <input type="button" onclick="forgetPassword()" value="forgot password"/> 
+        
+        
+        
+        
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Dropdown Menu for Mobile Devices -->
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">  <img src= "img/piechart.png" class="img-responsive BigLogo " alt=""></a>
+                </div>
 
-
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <script type="text/javascript" src="js/forgetPassword.js"></script>
-        </form>
+                <div class="collapse navbar-collapse" id="collapse">
+                    <div class="container-fluid">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="FirstPage.php">Home <span class=" glyphicon glyphicon-home"></span></a></li>
+                            <li><a href="#">About US <span class=" glyphicon glyphicon-user "></span></a></li>
+                            <li><a href="#">Sign Up <span class=" glyphicon glyphicon-pencil "></span></a></li>
+                            <li><a href="#">Contact Us <span class=" glyphicon glyphicon-envelope "></span></a> </li>
+                            <li><a href="login.php">Login <span class="glyphicon glyphicon-user"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        
+        
+        
+        
+        
+       
+           
+           
+    
+       <div class="container">
+           <div class="row all">
+               
+               <div class="Absolute-Center is-Responsive">
+                        <div class=" thumbnail BigPic">
+                            <img src= "img/BigImageLogin.png" class="img-responsive icons " alt="">
+                        </div>
+                   <h2 class="SignIN"> Sign in to your Account</h2>
+                   <div class="col-sm-12 col-md-10 col-md-offset-1">
+                       <form action="checkLogin.php" method="POST">
+                           <div class="form-group input-group">
+                               <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                               <input class="form-control"
+                                      type="text"
+                                      name="username"
+                                      placeholder="username"
+                                      value="<?php echo $username; ?>" />
+                               <span id="usernameError" class="error">
+                                   <?php
+                                   if (isset($errorMessage) && isset($errorMessage['username'])) {
+                                       echo $errorMessage['username'];
+                                   }
+                                   ?>
+                               </span>
+                           </div>
+                           <div class="form-group input-group">
+                               <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                               <input class="form-control"
+                                      type="password" 
+                                      name="password" 
+                                      placeholder="password"
+                                      value="" />
+                               <span id="passwordError" class="error">
+                                   <?php
+                                   if (isset($errorMessage) && isset($errorMessage['password'])) {
+                                       echo $errorMessage['password'];
+                                   }
+                                   ?>
+                               </span>
+                           </div>
+                           <div class="checkbox text-center">
+                               <label class="text-center">
+                                   <input type="checkbox"> Remember me
+                               </label>
+                           </div>
+                           <div class="form-group">
+                               <input type="submit" value="Login" name="login" class="btn btn-login btn-block"/>
+                           </div>
+                           <div class="form-group text-center">
+                               <a href="register.php"  > Register</a>&nbsp;|&nbsp;<a href="#" onclick="forgetPassword()" value="forgot password"/>Forgot Password?</a>
+                                 <script type="text/javascript" src="js/forgetPassword.js"></script>
+                           </div>
+                       </form>        
+                   </div>  
+    </div>    
+  </div>
+</div>
+    
+    
+    
+    
+    
+    
+    
     </body>
 </html>
--- register.php
+++ register.php
@@ -2,51 +2,109 @@
 <html>
     <head>
 
-        <meta charset="UTF-8">
-        <title></title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <meta charset="utf-8">
+        <title>My Website</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+        <script src="js/respond.js"></script>
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/font-awesome.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="styles/ihover.css" rel="stylesheet"> <!-- for the images and effects on them-->
+        <link href="styles/ihover.min.css" rel="stylesheet"> 
+        <link href='http://fonts.googleapis.com/css?family=Lato:400,900|Kreon' rel='stylesheet' type='text/css'>
+
+        <script type="text/javascript" src="js/customer.js"></script>
+        <link rel="icon" type="image/x-icon" href="Images/logo.ico">
+       
     </head>
-    <body>
-        <h1>Sign Up</h1>
-        <hr/>
-        <form id="registerForm" action="checkRegister.php" method="POST">
-            <table
-                border="1">
-                <tbody>
-                    <tr>
-                        <th>Username</th>
-                        <td>
-                            <input type="text" name="username" value="<?php
-                            if (isset($_POST) && isset($_POST['username'])) {
-                                echo $_POST['username'];
-                            }
-                            ?>" />
-                            <span id="usernameError" class="error">
-                                <?php
-                                if (isset($errorMessage) && isset($errorMessage['username'])) {
-                                    echo $errorMessage['username'];
-                                }
-                                ?>
-                            </span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Password</th>
-                        <td>
-                            <input type="password" name="password" value="" />
-                            <span id="passwordError" class="error">
-                                <?php
-                                if (isset($errorMessage) && isset($errorMessage['password'])) {
-                                    echo $errorMessage['password'];
-                                }
-                                ?>
-                            </span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Confirm Password</th>
-                        <td>
-                            <input type="password" name="password2" value="" />
+
+    <body class="BackgroundImage">
+       
+        
+         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Dropdown Menu for Mobile Devices -->
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">  <img src= "img/piechart.png" class="img-responsive BigLogo " alt=""></a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="collapse">
+                    <div class="container-fluid">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Home <span class=" glyphicon glyphicon-home"></span></a></li>
+                            <li><a href="#">About US <span class=" glyphicon glyphicon-user "></span></a></li>
+                            <li><a href="#">Sign Up <span class=" glyphicon glyphicon-pencil "></span></a></li>
+                            <li><a href="#">Contact Us <span class=" glyphicon glyphicon-envelope "></span></a> </li>
+                            <li><a href="login.php">Login <span class="glyphicon glyphicon-user"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        
+        
+        <div class="container">
+           <div class="row">
+               
+               <div class="Absolute-Center is-Responsive">
+                        <div class=" thumbnail BigPic">
+                            <img src= "img/Register.png" class="img-responsive icons " alt="">
+                        </div>
+                   <h2 class="Register"> Register With Us</h2>
+         <div class="col-sm-12 col-md-10 col-md-offset-1">
+                    <form id="registerForm" action="checkRegister.php" method="POST">
+                           <div class="form-group input-group">
+                               <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                               <input class="form-control"
+                                      type="text"
+                                      name="username"
+                                      placeholder="username"
+                                      value="<?php
+                                        if (isset($_POST) && isset($_POST['username'])) {
+                                            echo $_POST['username'];
+                                        }
+                                        ?>" />
+                                       
+                                        <span id="usernameError" class="error">
+                                            <?php
+                                            if (isset($errorMessage) && isset($errorMessage['username'])) {
+                                                echo $errorMessage['username'];
+                                            }
+                                            ?>
+                                        </span>
+                           </div>
+                           <div class="form-group input-group">
+                               <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                               <input class="form-control"
+                                      type="password" 
+                                      name="password" 
+                                      placeholder="password"
+                                      value="" />
+                               <span id="passwordError" class="error">
+                                   <?php
+                                   if (isset($errorMessage) && isset($errorMessage['password'])) {
+                                       echo $errorMessage['password'];
+                                   }
+                                   ?>
+                               </span>
+                           </div>
+                                
+                                <div class="form-group input-group">
+                               <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                               <input class="form-control"
+                                      type="password" 
+                                      name="password2" 
+                                      placeholder="Confirm password"
+                                      value="" />
                             <span id="password2Error" class="error">
                                 <?php
                                 if (isset($errorMessage) && isset($errorMessage['password2'])) {
@@ -54,19 +112,36 @@
                                 }
                                 ?>
                             </span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>
-                            <input type="submit" value="Register" name="register" />
-                            <input type="button" value="Return" name="Return to Log in" onclick="document.location.href = 'login.php'"  />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                           </div>
+                        
+                        
+                        
+                        
+                           
+                           <div class="form-group">
+                               <input type="submit" value="Register" name="register" class="btn btn-login btn-block"/>
+                             
+                       </form>
+                       
+                     <div class="form-group text-center GOBACK">
+                         <a href = "login.php">Go back</a>
+                     </div>
 
-        </form>
+                    </div>
+               </div>
+           </div>
+        </div>
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         <script type="text/javascript" src="js/register.js"></script>
     </body>
 </html>
